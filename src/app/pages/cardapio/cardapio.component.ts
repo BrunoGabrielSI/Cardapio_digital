c83a65c37a82ed454d71c8285b66157b9b184ed3
BrunoGabrielSI <99301997+BrunoGabrielSI@users.noreply.github.com>
2025-05-23 09:17:08 -0300

    refactor: ajustando a lógica do componente cardapio

--- src/app/pages/cardapio/cardapio.component.ts
+++ src/app/pages/cardapio/cardapio.component.ts
@@ -1,15 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Pipe, PipeTransform } from '@angular/core';
-import { CategoriaCorPipe } from '../../pipes/categoria-cor.pipe';
-
-@Component({
-  selector: 'app-cardapio',
-  standalone: true,
-  templateUrl: './cardapio.component.html',
-  styleUrls: ['./cardapio.component.scss'],
-  imports: [CommonModule, CategoriaCorPipe]
-})
+import { CategoriaCorPipe } from '../../pipes/categoria-cor.pipe'; // ajuste o caminho se necessário
 
 interface Prato {
   nome: string;
@@ -28,23 +19,23 @@ interface Prato {
 export class CardapioComponent {
 
   pratos: Prato[] = [
-    { nome: 'Pão', preco: 1.5, categoria: 'Entrada', disponivel: true },
-    { nome: 'Frango', preco: 20, categoria: 'Entrada', disponivel: true },
-    { nome: 'Crepe', preco: 16, categoria: 'Entrada', disponivel: false },
+    { nome: 'Milho', preco: 8.5, categoria: 'Entrada', disponivel: true },
+    { nome: 'Bolinho de costela', preco: 24, categoria: 'Entrada', disponivel: true },
+    { nome: 'Crepe de Frango', preco: 16, categoria: 'Entrada', disponivel: false },
     { nome: 'Lasanha', preco: 42, categoria: 'Principal', disponivel: true },
-    { nome: 'Bacon e Fritas', preco: 48.5, categoria: 'Principal', disponivel: false },
-    { nome: 'Frango Grelhado', preco: 36, categoria: 'Principal', disponivel: true },
-    { nome: 'Contra-filé', preco: 65.9, categoria: 'Principal', disponivel: true },
-    { nome: 'Palmito', preco: 39, categoria: 'Principal', disponivel: false },
-    { nome: 'Mandioca frita', preco: 22.9, categoria: 'Entrada', disponivel: false },
-    { nome: 'Pudim de Leite', preco: 12.5, categoria: 'Sobremesa', disponivel: true },
-    { nome: 'Sorvete', preco: 18, categoria: 'Sobremesa', disponivel: true },
-    { nome: 'Mousse de Uva', preco: 18, categoria: 'Sobremesa', disponivel: false },
+    { nome: 'Frango a Passarinho', preco: 48.5, categoria: 'Principal', disponivel: false },
+    { nome: 'Frango Grelhado', preco: 40, categoria: 'Principal', disponivel: true },
+    { nome: 'Picanha', preco: 76, categoria: 'Principal', disponivel: true },
+    { nome: 'Tutu de feijão', preco: 39, categoria: 'Principal', disponivel: false },
+    { nome: 'Coxinha de doce de leite', preco: 22.9, categoria: 'Sobremesa', disponivel: false },
+    { nome: 'Pudim', preco: 12.5, categoria: 'Sobremesa', disponivel: true },
+    { nome: 'Petit Gâteau', preco: 18, categoria: 'Sobremesa', disponivel: true },
+    { nome: 'Mousse de Uva', preco: 10, categoria: 'Sobremesa', disponivel: false },
     { nome: 'Refrigerante', preco: 6, categoria: 'Bebida', disponivel: true },
-    { nome: 'Suco Natural', preco: 8.5, categoria: 'Bebida', disponivel: true },
-    { nome: 'Água com Gás', preco: 4.5, categoria: 'Bebida', disponivel: true },
-    { nome: 'Vinho seco', preco: 32, categoria: 'Bebida', disponivel: false },
-    { nome: 'Cerveja', preco: 15, categoria: 'Bebida', disponivel: true }
+    { nome: 'Suco Natural', preco: 10, categoria: 'Bebida', disponivel: true },
+    { nome: 'Água com Gás', preco: 5, categoria: 'Bebida', disponivel: true },
+    { nome: 'Vinho', preco: 28, categoria: 'Bebida', disponivel: false },
+    { nome: 'Cerveja', preco: 12, categoria: 'Bebida', disponivel: true }
   ];
 
 }
